c4db5a220970516c97959eb402764dc03b9904e9
dark8ndev <andres_car098@hotmail.com>
2024-01-09 22:39:58 -0500

    Manejando estado de TodoSearch

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,12 +1,16 @@
+import React from 'react';
 import './TodoSearch.css'
 
 function TodoSearch() {
+    const [searchValue, setSearchValue] = React.useState('')
+
     return (
         <input
             placeholder="Buscar tarea ..."
             className="TodoSearch"
+            value={searchValue}
             onChange={(event) => {
-                console.log(event.target.value)
+                setSearchValue(event.target.value)
             }}
         />
     );
